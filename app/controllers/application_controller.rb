c77b68ffa445ed8f77c6f831f0d5f0bf2941e3be
mmsesay <maej_01@hotmail.com>
2022-02-21 01:13:27 +0000

    Refactor: login route Add: current_user and pass_current_user methods Add: login to create user Add: return for login user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    session[:current_user]
+  end
+
+  def pass_current_user(login_user)
+    session[:current_user] = login_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      get 'login', to: 'login#index'
+      resources :login, only: [:index, :create]
     end
   end
 end
