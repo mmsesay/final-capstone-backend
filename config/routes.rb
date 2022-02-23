80b050c68776091500380f9b6afd21b426e099d8
Collins Tatang <makungongcollins@gmail.com>
2022-02-23 14:17:29 +0100

    Add cars resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cars
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :login, only: [:index, :create]
---
+++ db/migrate/20220223122632_create_cars.rb
@@ -0,0 +1,16 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.string :model_info
+      t.integer :reservation_fee
+      t.integer :user_id
+
+      t.timestamps
+    end
+
+    add_index :cars, :user_id
+  end
+end
