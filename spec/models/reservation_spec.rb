811d449dc083716ea943656f3ea079eb8934e3f4
mmsesay <maej_01@hotmail.com>
2022-02-24 11:55:21 +0000

    Add: reservation model tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -8,18 +8,40 @@ RSpec.describe Reservation, type: :model do
     #   @car = Car.new(name: "Car #{i}", image: "photo #{i}", description: "description #{i}", model_info: "model #{i}", reservation_fee: "#{i}.00", user_id: @user.id)
     # end
 
-    # # @posts = Post.where(author_id: @user.id).all
+    # # # @posts = Post.where(author_id: @user.id).all
     # 3.times do |i|
     #   @reservation = Reservation.new(duration: "#{i}.to_s", user_id: @user.id, car_id: @car.id)
     # end
 
-    @reservation = Reservation.new(duration: "4", user_id: 1, car_id: 2)
+    @reservation = Reservation.new(duration: '4', user_id: 1, car_id: 2)
   end
 
   describe 'reservation validation tests' do
     it 'validates the presence of a duration' do
       @reservation.duration = nil
       expect(@reservation).to_not be_valid
+    end
+
+    it 'validates the presence of a car' do
+      @reservation.car_id = nil
+      expect(@reservation).to_not be_valid
+    end
+
+    it 'validates the presence of a user' do
+      @reservation.user_id = nil
+      expect(@reservation).to_not be_valid
+    end
+
+    it 'validates the duration match' do
+      expect(@reservation.duration).to eq 4
+    end
+
+    it 'validates the user_id match' do
+      expect(@reservation.user_id).to eq 1
+    end
+
+    it 'validates the car_id match' do
+      expect(@reservation.car_id).to eq 2
     end
   end
 end
