31c31e7d85387320494d67ea4b2bc1710cf2979a
mmsesay <maej_01@hotmail.com>
2022-02-24 11:57:36 +0000

    Add: reservation model tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,18 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   before(:each) do
-    # @car = Car.new(name: 'John', bio: 'I am a content creator', photo: '', posts_counter: 0)
-    # @user = User.new(username: 'John Doe')
-    # 5.times do |i|
-    #   @car = Car.new(name: "Car #{i}", image: "photo #{i}", description: "description #{i}", model_info: "model #{i}", reservation_fee: "#{i}.00", user_id: @user.id)
-    # end
+    @user = User.new(username: 'John Doe')
+    @car = Car.new(name: 'Car 1', image: 'photo_url', description: 'description 1', model_info: 'model 1', reservation_fee: '150', user_id: @user.id)
 
-    # # # @posts = Post.where(author_id: @user.id).all
-    # 3.times do |i|
-    #   @reservation = Reservation.new(duration: "#{i}.to_s", user_id: @user.id, car_id: @car.id)
-    # end
-
-    @reservation = Reservation.new(duration: '4', user_id: 1, car_id: 2)
+    @reservation = Reservation.new(duration: '4', user_id: @user.id, car_id: @car.id)
   end
 
   describe 'reservation validation tests' do
