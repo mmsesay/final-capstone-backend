a1347029de13bd5b49475df37171d430a3a201e8
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-02-27 12:11:57 +0000

    Add: cloudinary for saving in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,11 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
+  config.active_storage.service = :cloudinary
+
+  heroku config:set CLOUDINARY_URL='cloudinary://419568522474133:T7blRfjfKRhLWKXNoyh8QOJ51dM@maej'
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
