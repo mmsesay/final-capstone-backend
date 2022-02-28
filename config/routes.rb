2cd42a16292789bf9a0155e16e2fcbbc3606e546
AmineHLub <Amino.bhl@gmail.com>
2022-02-28 23:17:04 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  resources :cars
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :login, only: [:index, :create]
       delete 'logout', to: 'login#destroy'
-      get 'cars', to: 'cars#index'
-      resources :users, only: [:index] do
-        get 'reservations', to: 'reservations#show'
-        resources :cars do
-          resources :reservations
-        end
-      end
+      resources :cars, defaults: { format: 'json' }
+      resources :reservations, defaults: { format: 'json' }
     end
   end
 end
